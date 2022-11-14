9e2d2874ed59c4d035c99e769e801b064516761a
Chris Waddell <yesitschris@hotmail.com>
2022-11-15 00:42:58 +1100

    Initialize

---
+++ README.md
@@ -0,0 +1,45 @@
+#Console Log Plus
+Console log Plus adds color and size formatting to a browser console.log message. It also automatically adds function name and function arguments to the
+// start of the message to make it easier to debug. (This only works for non-strict mode.)
+
+
+## How to use it
+In your HTML file. Put at the top of the script tags
+``
+<script src="./log.js"> </script>
+``
+
+The console.log message is this format:
+``
+console.log('%c' + log.fnName(...arguments) + ' -  message', log.logType('info'))
+``
+
+IF it's a console.log messge on the global scope, add:
+``
+console.log('%c' + ' -  message', log.logType('info'))
+``
+
+It's a good idea to add a code snippet to Visual Studio to easily add this console log format. Instead of typing out ``console.log(xxxxx)``, type out ``jco``.
+
+You could add the following to your javascript.json file:
+
+``
+"Log with extra features": {
+    "prefix": "jco",
+	"body": [
+        "console.log('%c' + log.fnName(...arguments) + ' - ${1}', log.logType('info'))"
+	],
+	"description": "Log with extra features. It shows function name and arguments. Use debugger or breakpoint if applicable."
+},
+"Log with extra features Global": {
+	"prefix": "jcog",
+	"body": [
+        "console.log('%c' + ${1}', log.logType('info'))"
+	],
+	"description": "Log with extra features globally. Use debugger or breakpoint if applicable."
+}
+``
+
+
+You can add settings for the message color and size.
+
---
+++ log.js
@@ -0,0 +1,92 @@
+// Console log Plus adds color and size formatting to a console.log message
+// It also automatically adds function name and function arguments to the
+// start of the message to make it easier to debug. (This only works for
+// non-strict mode.)
+
+
+// Usage
+// console.log("%c" + log.fnName(...arguments) + " - message", log.logType("new"))
+// In the global scope:
+// console.log("%c" + " - message", log.logType("new"))
+
+
+
+let log = function () {
+
+    // ^SETTINGS
+    // Put your log type settings in here
+    // Font color, font size
+    const logLevelList = [{
+            logType: "info",
+            description: "Information",
+            color: "#4499ff",
+            size: 15
+        },
+        {
+            logType: "new",
+            description: "Working on code temporarily, fix and move on",
+            color: "#22aa22",
+            size: 17
+        },
+        {
+            logType: "watch",
+            description: "To watch variables",
+            color: "#22ffdd",
+            size: 15
+        }
+    ]
+
+
+    function logCSS(c, size) {
+        let fs
+        try {
+            fs = Number(size) || 22;
+        } catch {
+            fs = 22
+        }
+        let color = c || "#4444ff";
+        return `color: ${color}; font-size: ${fs}px`
+    }
+
+
+    function logTypeFind(lType) {
+        return logLevelList.find(logLevel => logLevel.logType === lType)
+    }
+
+    function logType(lType) {
+        let logobj = logTypeFind(lType)
+        return (logobj) ? logCSS(logobj.color, logobj.size) : logCSS()
+    }
+
+
+    function fnName(...args) {
+        let logFnName = ""
+        try {
+            logFnName = (fnName.caller != null) ? fnName.caller.name : ""
+        } catch {
+            logFnName = ""
+        }
+
+        let argList = ""
+        try {
+            if (args != null) {
+                args.forEach(el => {
+                    if (el) {
+                        if (argList) argList += ", "
+                        argList += (el.toString() === "[object Object]") ? JSON.stringify(el) : el.toString()
+                    }
+                })
+            }
+        } catch {
+            argList = ""
+        }
+
+        return logFnName + "(" + argList + ")"
+    }
+
+
+    return {
+        logType,
+        fnName
+    }
+}();
